43f9b9defeed6c2e3286a632ef309d7add9119d2
SravyaJanga <94519766+SravyaJanga@users.noreply.github.com>
2021-12-23 10:52:05 +0530

    Add files via upload

---
+++ JDBC/EmpDao.java
@@ -0,0 +1,10 @@
+package com.sonata.Dao;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.List;
+import com.sonata.Model.Employee;
+public interface EmpDao {
+	public int save(Object object);
+	public List<Employee> getData();
+
+}
